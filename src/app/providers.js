8f07e43507147644d923f4e5bc196ce9e627b536
Seif Ahmed <seifahmed11125@gmail.com>
2026-05-01 04:21:02 +0300

    cleanup: remove individual CSS files

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,11 +1,6 @@
 'use client'
 
 import '../styles/globals.css'
-import '../components/About.css'
-import '../components/Contact.css'
-import '../components/Hero.css'
-import '../components/Portfolio.css'
-import '../components/Services.css'
 
 export default function Providers({ children }) {
   return <>{children}</>
