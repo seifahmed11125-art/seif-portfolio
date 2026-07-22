0a3e6cdac287c4e690b822da7364f8b617693427
Seif <seifahmed11125@gmail.com>
2026-07-22 12:04:19 +0300

    Show only 4 projects on landing page

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -56,7 +56,7 @@ const Portfolio = () => {
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
         >
           <AnimatePresence mode='popLayout'>
-            {filteredProjects.map((project) => (
+            filteredProjects.slice(0, 4).map
               <motion.div
                 key={project.id}
                 layout
